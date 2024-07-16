09c5b6f2336b9fb2f3cd53692cdc1bf0a8464047
Deepak Kumar <deepaklg02@gmail.com>
2024-07-16 22:41:40 +0530

    Added Post Login Screen

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Home from "./components/Home";
-import Post from './components/Post';
+import Post from "./components/Post";
+import { useUser } from "./userContext.js";
 const App = () => {
+  const { user } = useUser();
   return (
     <div className="inter">
       <BrowserRouter>
@@ -14,9 +16,16 @@ const App = () => {
           <Route path="/" element={<Caraousel />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/login-successful" element={<Post/>} />
-          <Route path="/home" element={<Home/>} />
-          <Route path="*" element={<h1>404 Not Found</h1>} />
+          {user && (
+            <>
+              <Route path="/login-successful" element={<Post />} />
+              <Route path="/home" element={<Home />} />
+            </>
+          )}
+          <Route
+            path="*"
+            element={<h1 className="text-4xl">404 Not Found</h1>}
+          />
         </Routes>
       </BrowserRouter>
     </div>
